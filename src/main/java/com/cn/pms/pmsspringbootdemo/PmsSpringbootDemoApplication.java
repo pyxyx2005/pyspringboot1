2daee217dcde08db0cf86b6fff65c784c78c7369
pySpringboots <49668047@qq.com>
2020-03-05 20:36:12 +0800

    addRead

---
+++ src/main/java/com/cn/pms/pmsspringbootdemo/PmsSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cn.pms.pmsspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsSpringbootDemoApplication.class, args);
+    }
+
+}
