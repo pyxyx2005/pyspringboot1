a8a7bd0c9b8c13b6f54dca0faa654e0222a437c6
pySpringboots <49668047@qq.com>
2020-03-08 09:34:57 +0800

    add index and bootstrap

---
+++ src/main/java/com/cn/pms/pmsspringbootdemo/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.cn.pms.pmsspringbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public  String index()
+    {
+        return "index";
+    }
+}
